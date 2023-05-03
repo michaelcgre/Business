99fe05a68ffcee974a9b0c9fbf0262d1ca539d23
michaelcgre <124597287+michaelcgre@users.noreply.github.com>
2023-05-03 14:58:04 -0400

    Initial

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    var heroSection = document.getElementById('hero-section');
+    var heroText = heroSection.querySelector('.hero-text');
+    heroText.classList.add('fade-in');
+  }
+
